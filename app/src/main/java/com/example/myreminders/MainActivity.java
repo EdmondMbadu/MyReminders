047c892bea073aab0f29a55ad20843fc4406947c
Edmond Mbadu <mbadungoma@gmail.com>
2019-09-26 14:52:25 -0400

    This is the second version of myreminders. I think.

--- app/src/main/java/com/example/myreminders/MainActivity.java
+++ app/src/main/java/com/example/myreminders/MainActivity.java
@@ -3,26 +3,35 @@ package com.example.myreminders;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Intent intent;
+    DBHandler dbHandler;
+
+    ListView myReminderListsView;
+    MyRemindersLists myRemindersListsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        dbHandler = new DBHandler(this, null);
+
+//        myReminderListsView=(ListView)findViewById(R.id.myReminderListsView) ;
+        myRemindersListsAdapter= new MyRemindersLists(this, dbHandler.getMyReminders(),0);
+        myReminderListsView.setAdapter(myRemindersListsAdapter);
 
 
     }
@@ -45,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
                return true;
            case R.id.action_add_reminder:
-               intent= new Intent(this, CreateReminders.class);
+               intent= new Intent(this, AddReminder.class);
+               startActivity(intent);
                return true;
                default:
                    return super.onOptionsItemSelected(item);
@@ -56,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCreateReminders(View view){
 
-    intent= new Intent(this,CreateReminders.class);
+    intent= new Intent(this, AddReminder.class);
     startActivity(intent);
 
     }
